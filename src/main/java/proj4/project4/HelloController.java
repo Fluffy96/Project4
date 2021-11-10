94918510bd3d2c6c138337f1fb21db3e25858dba
Fluffy96 <52479039+Fluffy96@users.noreply.github.com>
2021-11-10 14:48:53 -0500

    pizza

--- src/main/java/proj4/project4/HelloController.java
+++ src/main/java/proj4/project4/HelloController.java
@@ -1,14 +1,19 @@
 package proj4.project4;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button hell;
+    @FXML
+    private TextField cNum;
+
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        cNum.setText("Welcome to JavaFX Application!");
     }
 }
