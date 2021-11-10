263ab9156eca486606b6e772c3d2d56f3e304bc8
Fluffy96 <52479039+Fluffy96@users.noreply.github.com>
2021-11-10 17:47:09 -0500

    Added the three pizza classes

--- src/main/java/proj4/project4/Deluxe.java
+++ src/main/java/proj4/project4/Deluxe.java
@@ -1,11 +1,53 @@
 package proj4.project4;
 
+import java.util.Arrays;
+
 public class Deluxe extends Pizza{
 
     public static final double BASEPRICE = 12.99;
+    private double price;
+
+    Deluxe(String pSize){
+        if(pSize.toLowerCase().equals( "small")){
+            size = Size.SMALL;
+            setprice(BASEPRICE+size.getPrice());
+        }else if(pSize.toLowerCase().equals("medium")){
+            size = Size.MEDIUM;
+            setprice(BASEPRICE+size.getPrice());
+        }else{
+            size = Size.LARGE;
+            setprice(BASEPRICE+size.getPrice());
+        }
+        Topping[] otherList = new Topping[] {Topping.MUSHROOM, Topping.CHICKEN, Topping.OLIVES, Topping.ONION, Topping.PEPPERONI};
+        toppings.addAll(Arrays.asList(otherList));
+    }
+    @Override
+    public double getprice() {
+        return pizzaPrice;
+    }
 
     @Override
-    public double price() {
-        return 0;
+    public void setprice(double cost) {
+        pizzaPrice = pizzaPrice + cost;
     }
+
+    @Override
+    public void addTopping(Topping top){
+        toppings.add(top);
+    }
+
+    @Override
+    public void removeTopping(Topping top){
+        toppings.remove(top);
+    }
+
+    @Override
+    public String[] getTopppings(){
+        String[] topList = new String[toppings.size()];
+        for(int i = 0; i < toppings.size();i++){
+            topList[i] = toppings.get(i).toString();
+        }
+        return topList;
+    }
+
 }
--- src/main/java/proj4/project4/Pepperoni.java
+++ src/main/java/proj4/project4/Pepperoni.java
@@ -1,11 +1,54 @@
 package proj4.project4;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 public class Pepperoni extends Pizza{
 
     public static final double BASEPRICE = 8.99;
 
-    @Override
-    public double price() {
-        return 0;
+    Pepperoni(String pSize){
+        if(pSize.toLowerCase().equals( "small")){
+            size = Size.SMALL;
+            setprice(BASEPRICE+size.getPrice());
+        }else if(pSize.toLowerCase().equals("medium")){
+                size = Size.MEDIUM;
+                setprice(BASEPRICE+size.getPrice());
+        }else{
+            size = Size.LARGE;
+            setprice(BASEPRICE+size.getPrice());
+        }
+        toppings.add(Topping.PEPPERONI);
     }
+
+    @Override
+    public double getprice() {
+        return pizzaPrice;
+    }
+
+    @Override
+    public void setprice(double cost) {
+        pizzaPrice = pizzaPrice + cost;
+    }
+
+    @Override
+    public void addTopping(Topping top){
+        toppings.add(top);
+    }
+
+    @Override
+    public void removeTopping(Topping top){
+        toppings.remove(top);
+    }
+
+    @Override
+    public String[] getTopppings(){
+        String[] topList = new String[toppings.size()];
+        for(int i = 0; i < toppings.size();i++){
+            topList[i] = toppings.get(i).toString();
+        }
+        return topList;
+    }
+
 }
+
