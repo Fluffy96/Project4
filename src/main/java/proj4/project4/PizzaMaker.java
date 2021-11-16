779496dd588490114845083b6ffeb7bc55af18b9
DivyeshBaskaran <divyesh.baskaran@gmail.com>
2021-11-15 19:13:58 -0500

    7:13 pm 11/15/2021 commit

---
+++ src/main/java/proj4/project4/PizzaMaker.java
@@ -0,0 +1,18 @@
+package proj4.project4;
+
+import java.util.Locale;
+
+public class PizzaMaker {
+    public static Pizza createPizza(String flavor){
+        switch (flavor.toLowerCase()){
+            case "deluxe":
+                return new Deluxe("small");
+            case "hawaiian":
+                return new Hawaiian("small");
+            case "pepperoni":
+                return new Pepperoni("small");
+            default:
+                return new Deluxe("small");
+        }
+    }
+}
