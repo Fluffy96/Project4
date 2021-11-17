10cad488b9ff21a129f8fa38d7ff6fe9a526070c
Fluffy96 <52479039+Fluffy96@users.noreply.github.com>
2021-11-16 22:49:44 -0500

    FINAL PUSH

--- src/main/java/proj4/project4/Size.java
+++ src/main/java/proj4/project4/Size.java
@@ -1,10 +1,23 @@
 package proj4.project4;
+/**
+ * This class is the UI that contains the run function to interract with album collections
 
+ * @author Divyesh Nemam Baskaran, Viraj Patel
+ *
+ */
+
+/**
+ * Method for size object
+ */
 public enum Size {
     SMALL(0.00), MEDIUM(2.00), LARGE(4.00);
 
     private double price;
 
+    /**
+     * Sets
+     * @param price
+     */
     Size(double price){
         this.price = price;
     }
--- src/main/java/proj4/project4/StoreOrders.java
+++ src/main/java/proj4/project4/StoreOrders.java
@@ -1,10 +1,18 @@
 package proj4.project4;
+/**
+ * This class is the UI that contains the run function to interract with album collections
 
+ * @author Divyesh Nemam Baskaran, Viraj Patel
+ *
+ */
 import javafx.beans.Observable;
 import javafx.collections.ObservableList;
 
 import java.util.ArrayList;
 
+/**
+ * Store Order object method
+ */
 public class StoreOrders {
 
     private static ArrayList<ObservableList<Orders>> finalOrders = new ArrayList<>();
@@ -13,22 +21,41 @@ public class StoreOrders {
     private static ArrayList<String> phoneNumberList = new ArrayList<>();
     private static String currentNumber;
     private static final int NOEXSIST = -1;
-    public StoreOrders(){
 
+    /**
+     * Generic constructor method for StoreOrders
+     */
+    public StoreOrders(){
     }
 
+    /**
+     * Adds an order to the order arraylist
+     * @param order
+     */
     public void addOrders( Orders order){
         orders.add(order);
     }
 
+    /**
+     * Adds the Total Price of the order into the Total price array
+     * @param totPrice
+     */
     public void addTP(double totPrice){
         totalPrices.add(totPrice);
     }
 
+    /**
+     * Returns all orders that were added
+     * @return Orders
+     */
     public  ArrayList<Orders> getOrders() {
         return orders;
     }
 
+    /**
+     * Takes in an order that user wants to remove and removes it from the orders array
+     * @param number
+     */
     public void removeOrder(String number){
         int index = phoneNumberList.indexOf(number);
         phoneNumberList.remove(index);
@@ -36,18 +63,35 @@ public class StoreOrders {
         orders.remove(index);
     }
 
+    /**
+     * Method returns to user the phonenumbers of all the orders
+     * @return phonenUmberList
+     */
     public  ArrayList<String> getPhoneNumberList() {
         return phoneNumberList;
     }
 
+    /**
+     * Adds a phone number of the order into the phone number array
+     * @param num
+     */
     public void addPhoneNumbers (String num){
         phoneNumberList.add(num);
     }
 
+    /**
+     * Method reurnts the tottal price arraylist
+     * @return totalPrices
+     */
     public  ArrayList<Double> getTotalPrices() {
         return totalPrices;
     }
 
+    /**
+     * Method returns whether a phone number is in orders
+     * @param num
+     * @return Boolean
+     */
     public boolean isIn(String num){
         if(phoneNumberList.indexOf(num) != NOEXSIST){
              return true;
@@ -56,21 +100,12 @@ public class StoreOrders {
         }
     }
 
-    public  void setCurrentNumber(String currentNumber) {
-        StoreOrders.currentNumber = currentNumber;
-    }
-
+    /**
+     * To String Method for Store Orders Object
+     * @return
+     */
     @Override
     public String toString(){
         return null;
     }
-
-    public void addOrder(Orders order){
-
-    }
-
-    public void export(){
-
-    }
-
 }
